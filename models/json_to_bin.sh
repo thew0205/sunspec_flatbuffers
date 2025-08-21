9f2a7462da8ecc87768661e88460f3e4417c25cc
Matthew-0205 <matthew@powerlabstech.com>
2025-08-21 03:09:39 +0100

    Added Models with lint to remote

---
+++ models/json_to_bin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+FLATC_COMPILER="/home/busoye_tm/Documents/test/flatbuffers/build/flatc"
+if [[ ! -f ${FLATC_COMPILER} ]]; then
+    echo "Error no Flatbuffer compiler found"
+    exit 1
+fi
+
+mkdir -p models/flatbuffer/json
+mkdir -p models/flatbuffer/annotate_binary
+
+${FLATC_COMPILER} --cpp -o sunspec/include sunspec_model_definition.fbs 
+python3 models/model_to_flatbuffer_json.py
+
+for model in 1 101 102 103 111 112 113 120 126 160
+do  
+    ${FLATC_COMPILER} --binary -o models/flatbuffer/binary sunspec_model_definition.fbs "models/flatbuffer/json/model_${model}.json"
+    xxd -i -n modelDefinition${model} models/flatbuffer/binary/model_${model}.bin  > sunspec/src/models/flatbuffer_array_model_${model}.cpp
+done
+${FLATC_COMPILER} -o models/flatbuffer/ATT  --annotate  sunspec_model_definition.fbs models/flatbuffer/binary/model_1.bin
